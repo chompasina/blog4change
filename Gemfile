fa772600c52608ed7455859da0e2cc51bcf0ec21
Tommasina <tommasina1@gmail.com>
2016-11-12 11:48:54 -0700

    Adds user can create a post feature test, post controller and view

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
+# gem 'bootstrap-sass', '~> 3.2.0'
+gem 'bootstrap', '~> 4.0.0.alpha3'
+
 gem 'rails_12factor', group: :production
 
 group :development, :test do
---
+++ spec/features/user_creates_post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "User can create a post", type: :feature do
+  scenario "they visit the root" do    
+    visit root_path
+    click_link "Create new post"
+    expect(page).to have_content("Author")
+    expect(page).to have_content("Body")
+    expect(page).to have_content("Title")
+    expect(page).to have_button("Create Post")
+    
+    fill_in "Author", with: "Tommasina"
+    fill_in "Title", with: "My wonderful post"
+    fill_in "Body", with: "Yada yada yada"
+    
+    click_button "Create Post"
+  end
+end
