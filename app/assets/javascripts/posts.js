76c2ba2313f02b0da3865cad9176087b9997e59b
Tommasina <tommasina1@gmail.com>
2016-11-12 17:08:45 -0700

    WIP still working on adding comments to page

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   viewDrafts();
   viewDefault();
   addComment();
-  storeCommentName();
-  storeCommentBody();
+  // storeCommentName();
+  // storeCommentBody();
   addCommentField();
 })
 
@@ -51,30 +51,33 @@ function addCommentField(){
       "<br><div class='comment-post' data-id='" +
       $(this).data('id') +
       "'><h6>Add your comment below:</h6> "+
-      "<form class='comment-form'>Name:<input type='text' name='name' id='comment-name'><br>Comment:<input type='text-area' name='comment-field' id='comment-body'>" +
+      "<form class='comment-form'>Name:<input type='text' name='name' class='comment-name-'" + 
+      $(this).data('id')+
+      "'><br>Comment:<input type='text-area' name='comment-field' class='comment-body'>" +
       "<br><input type='button' value='Submit' class='add-comment-button'></input></form></div>"
     );
     }    
   )
 }
 
-function storeCommentName(){
-  $(".post").on('keyup', '#comment-name', function(e){
-    var name = e.target.value;
-  })
-}
-function storeCommentBody(){
-  $(".post").on('keyup', '#comment-body', function(e){
-    var body = e.target.value;
-  })
-}
+// function storeCommentName(){
+//   $(".post").on('keyup', '#comment-name', function(e){
+//     var name = e.target.value;
+//   })
+// }
+// function storeCommentBody(){
+//   $(".post").on('keyup', '#comment-body', function(e){
+//     var body = e.target.value;
+//   })
+// }
 
 function addComment(){  
   $('.post').on('click', '.add-comment-button', function(e){
-  
+    debugger;
+    console.log($(".comment-name").val())
     var commentParams = {
-        name: $("#comment-name").val(),
-        body: $("#comment-body").val(),
+        name: $(".comment-name").val(),
+        body: $(".comment-body").val(),
         post_id: $(".comment-post").val()
       };
       
